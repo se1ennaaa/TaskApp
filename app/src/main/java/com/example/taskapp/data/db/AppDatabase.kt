21d59bc6d49164adbffc9c94e55fe0edfb137535
Geeks <altynaidjan@gmail.com>
2023-05-12 16:13:02 +0600

    hw4

--- app/src/main/java/com/example/taskapp/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/taskapp/data/db/AppDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.appleadd.model.Task
 import com.example.taskapp.data.db.TaskDao
-
+//Database
 @Database(entities = [Task::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
